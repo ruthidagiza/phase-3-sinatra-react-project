8efb5b0a1aa3101f2289d97a20004da62eab140e
ruthidagiza <ruthidagiza@gmail.com>
2022-09-14 07:28:00 +0300

    added articles api  routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,34 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/articles" do
+    articles=Article.all
+    articles.to_json
+  end
+  post "/articles" do
+  article=Article.create(
+    name:params[:name],
+    photo_url:params[:photo_url],
+    nationality:params[:nationality]
+  )
+  article.to_json
+  end
+  patch "/article/:id" do
+    article=Article.find(params[:id])
+    article.update(
+    name:params[:name],
+    photo_url:params[:photo_url],
+    nationality:params[:nationality]
+  )
+  article.to_json
+  end
+  delete "/article/:id" do
+  article=Article.find(params[:id])
+  article.destroy
+  article.to_json
   end
 
 end
+
+require './models'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+articles = [
+  {title: 'Why Facts Don’t Change Our Minds', content: 'he economist J.K. Galbraith once wrote, “Faced with a choice between changing one’s mind and proving there is no need to do so, almost everyone gets busy with the proof.”
+    ', writer: 'J.K. Galbraith', createdOn:"1663107511921",likes:2,},
+]
+articles.each do |u|
+    Article.create(u)
 
 puts "✅ Done seeding!"
